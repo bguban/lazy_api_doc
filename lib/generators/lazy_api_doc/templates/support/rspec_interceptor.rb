87946d4ca9e90f8dca7046fa842bd6ad77b9c9a4
Bogdan Guban <biguban@gmail.com>
2020-04-09 00:47:05 +0300

    documentation added.

--- lib/generators/lazy_api_doc/templates/support/rspec_interceptor.rb
+++ lib/generators/lazy_api_doc/templates/support/rspec_interceptor.rb
@@ -5,7 +5,7 @@ module LazyApiDocInterceptor
     %w[get post patch put head delete].each do |method|
       define_method(method) do |*args|
         result = super(*args)
-        # self.class.metadata[:doc]
+        # self.class.metadata[:doc] can be used to document only tests with doc: true metadata
         LazyApiDoc.add_spec(self)
         result
       end
